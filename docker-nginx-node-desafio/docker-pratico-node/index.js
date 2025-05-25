66431d9b9063abf2cfcb778b702fd625c3b4d42f
DanielHermesGT <danielhermesporto33@gmail.com>
2025-05-25 15:13:23 -0300

    chore: feito

---
+++ docker-nginx-node-desafio/docker-pratico-node/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const config = {
+    host: 'db', //nome do service do docker-compose
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Erro ao conectar ao banco de dados', err);
+    } else {
+        console.log('Conexão estabelecida com sucesso');
+    }
+});
+
+app.get('/', (req, res) => {
+  const insertSql = 'INSERT INTO people (name) VALUES ("Daniel")';
+  connection.query(insertSql, (insertErr) => {
+      if (insertErr) {
+          console.error('Erro ao inserir dados', insertErr);
+          return res.status(500).send('Erro ao inserir dados');
+      }
+
+      const selectSql = 'SELECT * FROM people';
+      connection.query(selectSql, (selectErr, results) => {
+          if (selectErr) {
+              console.error('Erro ao selecionar dados', selectErr);
+              return res.status(500).send('Erro ao selecionar dados');
+          }
+
+          const listItems = results.map(person => `<li>${person.name}</li>`).join('');
+          res.send(`<h1>Full Cycle Rocks!</h1><ul>${listItems}</ul>`);
+      });
+  });
+});
+app.listen(port, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
+});
